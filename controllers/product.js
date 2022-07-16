5ea2c75ad9d9abc91000f8869de4a3e1e19df806
Abasibiangake James <jamesabasibiangake@gmail.com>
2022-07-16 11:48:07 +0100

    draft commit(Abasibiangake)
    Add images and templates for the website

---
+++ controllers/product.js
@@ -0,0 +1,26 @@
+// create a reference to the model
+let ProductModel = require('../models/product');
+
+// Gets all products from the Database and renders the page to list them all.
+module.exports.usedProducts = function(req, res, next) {  
+    //outputs all the documents in the collection in an array
+    //has two object - one for error(err) and the other for success(usedProduct)
+    ProductModel.find((err, usedProducts) => {
+        console.log(usedProducts);
+        if(err)
+        {
+            return console.error(err);
+        }
+        else
+        {
+            console.log(usedProducts);
+            //send data from atlas db to the webpage
+           
+            res.render('products/list', {
+                title: 'List Of Used Product', 
+                ProductList: usedProducts
+                
+            })            
+        }
+    });
+}
